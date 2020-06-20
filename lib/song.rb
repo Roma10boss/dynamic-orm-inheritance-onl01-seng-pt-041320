2ac2949299793c5625c6ef4589d05310a1796333
Romario Gustave <romagustave10@gmail.com>
2020-06-20 13:24:42 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,8 @@
-require_relative "./interactive_record.rb"
+require_relative "../config/environment.rb"
+require 'active_support/inflector'
+require 'interactive_record.rb'
 
-class Song < InteractiveRecord
+class Student < InteractiveRecord
 
   self.column_names.each do |col_name|
     attr_accessor col_name.to_sym
